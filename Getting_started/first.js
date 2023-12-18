0e19ed2df2fb4e195b6d85250c6359e139fd6a54
Mohankrish08 <mohankrishce@gmail.com>
2023-12-18 08:05:18 +0530

    hello world

---
+++ Getting_started/first.js
@@ -0,0 +1,17 @@
+// print hello world in javascript
+
+console.log('Hello world')
+
+// basics arthimetic operations
+
+// addition
+console.log(5 + 5) 
+
+// subtraction
+console.log(8 - 4)
+
+// multiplicaton
+console.log(5 * 5)
+
+// division 
+console.log(10 / 2)
