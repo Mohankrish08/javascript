643745f1d9dc84e0a22a0c41f5230e66928e2b12
Mohankrish08 <mohankrishce@gmail.com>
2023-12-18 08:08:34 +0530

    js basics

---
+++ JavaScript basics/operators and experessions.js
@@ -0,0 +1,29 @@
+// Artimetic operators
+
+let a = 10
+let b = 5
+let sum = a + b
+let difference = a - b
+let product = a * b
+let quotient = a / b
+let remainder = a % b
+
+console.log(sum, difference, product, quotient, remainder)
+
+// comparison operators
+
+let isEqual = (a==b)
+let isGreater = (a>b)
+let isEqualOrLess = (a<=b)
+
+console.log('-----------')
+console.log(isEqual, isGreater, isEqualOrLess)
+
+// logical operators
+
+let AndOp = (a && b)
+let OrOp = (a||b)
+let notOp = (!a)
+
+console.log('--------------')
+console.log(AndOp, OrOp, notOp)
