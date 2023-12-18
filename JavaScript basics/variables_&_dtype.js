c4d23306e79d0fd4c47586b8401b025f27ec975e
Mohankrish08 <mohankrishce@gmail.com>
2023-12-18 08:08:53 +0530

    js basics

---
+++ JavaScript basics/variables_&_dtype.js
@@ -0,0 +1,27 @@
+// variables 
+
+let gretting = "hello"  // using let it allows reassignment
+var age = 20            // using var it declare for the whole program
+const pi = 3.14         // using const, the values can't be changed, immutable
+
+console.log(gretting, '|' ,typeof gretting)
+console.log(age, '|' ,typeof age)
+console.log(pi, '|' ,typeof pi)
+
+// data types
+
+let message = 'hi there'    // string
+let number = 8              // int
+let num = 90.2              // float
+let isLogged = true         // Boolean
+let empty =  null           // null
+let undefined               // undefined
+
+console.log('-----------------')
+console.log(message,'|' ,typeof message)
+console.log(number, '|' ,typeof number)
+console.log(num, '|' ,typeof num)
+console.log(isLogged,'|' , typeof isLogged)
+console.log(empty,'|' , typeof empty)
+console.log(undefined,'|' , typeof empty)
+
