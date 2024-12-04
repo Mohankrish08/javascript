c322f8e9b0123556e1a7673b9504974e56a1336f
Mohankrish08 <mohankrishce@gmail.com>
2024-12-04 07:40:14 +0530

    variables

--- Practice/test.js
+++ Practice/test.js
@@ -1,1 +1,5 @@
 document.getElementById("variable").innerHTML = "This is a testing variable!!";
+
+let firstName = "John";
+let lastName = "Doe";
+document.getElementById("multi").innerHTML = firstName + "<br>" + lastName;
